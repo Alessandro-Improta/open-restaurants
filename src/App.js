b861f948acfe881fd5d76cfb61c256ab0a63a238
Alessandro Improta <alessandro@gmetrix.com>
2021-07-06 23:31:19 -0600

    done

--- src/App.js
+++ src/App.js
@@ -9,20 +9,40 @@ function App() {
   const [ selectedDate, setSelectedDate ] = useState(new Date());
 
   useEffect(() => {
-    console.log(selectedDate);
-    console.log(typeof selectedDate.getDay());
-    console.log(typeof selectedDate.getHours());
-    console.log(typeof selectedDate.getMinutes());
-
-
-
+    filterList();
   }, [selectedDate])
 
-  console.log(parsedData);
+  function filterFunction(element) {
+    let year = selectedDate.getFullYear();
+    let month = selectedDate.getMonth();
+    let date = selectedDate.getDate();
+    let day = selectedDate.getDay();
+    let hours = selectedDate.getHours();
+
+    let start = element.times[day].start;
+    let end = element.times[day].end;
+    
+    if (element.times[day] && element.times[day].open) {
+      let startDate = new Date(year, month, start.hours > 12 && end.hours < 4 && hours < 4 ? date - 1 : date, start.hours, start.minutes);
+      let endDate = new Date(year, month, start.hours > 12 && end.hours < 4 && hours > 4 ? date + 1 : date, end.hours, end.minutes);
+      if (startDate.getTime() < selectedDate.getTime() && endDate.getTime() > selectedDate.getTime()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function filterList() {
+    let newList = parsedData.filter(restaurant => filterFunction(restaurant));
+    setFilteredList(newList);
+  }
+
   return (
     <>
       <DateTimePicker onChange={setSelectedDate} value={selectedDate} />
-      {}
+      {filteredList.map((restaurant, index) => (
+        <p key={"key"+index}>{restaurant.name}</p>
+      ))}
     </>
   );
 }
--- src/parsedData.js
+++ src/parsedData.js
@@ -1,98 +1,65 @@
-const json = require("./rest_hours.json");
+const restHours = require("./rest_hours.json");
 const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-const newTimes = {
-    0: {
-        open: false,
-        start: null,
-        end: null
-    },
-    1: {
-        open: false,
-        start: null,
-        end: null
-    },
-    2: {
-        open: false,
-        start: null,
-        end: null
-    },
-    3: {
-        open: false,
-        start: null,
-        end: null
-    },
-    4: {
-        open: false,
-        start: null,
-        end: null
-    },
-    5: {
-        open: false,
-        start: null,
-        end: null
-    },
-    6: {
-        open: false,
-        start: null,
-        end: null
-    }
-};
+class Times{
+    constructor() {
+        this["0"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["1"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["2"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["3"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["4"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["5"] = {
+            open: false,
+            start: null,
+            end: null
+        }
+        this["6"] = {
+            open: false,
+            start: null,
+            end: null
+        }
 
-console.log(json);
+    } 
+} 
 
 function parseRestaurantData(data) {
     let dataCopy = [...data];
     for (let restaurantIndex = 0; restaurantIndex < dataCopy.length; restaurantIndex++ ) {
         let timesArray = [...dataCopy[restaurantIndex].times]
         dataCopy[restaurantIndex].times = createNewTimesObject(timesArray);
-        
-        // restaurant.times = newTimesObj;
     }
     return dataCopy;
 }
 
+
 function createNewTimesObject(timesArray) {
-    let newTimesObj = {...newTimes};
+    let newTimesObj = new Times();
     for (let timesIndex = 0; timesIndex < timesArray.length; timesIndex++) {
         let time = timesArray[timesIndex];
-        let arraysOfDaysByNumber = [];
-        let hours = [];
-        let splitString = time.toLowerCase().split(' ');
-        for (let stringIndex = 0; stringIndex < splitString.length; stringIndex++) {
-            let string = splitString[stringIndex];
-            if (string.includes('-') && string.length > 1) {
-                let indexOfDash = string.indexOf('-');
-                let start = days.indexOf(string.substr(indexOfDash - 3, 3));
-                let end = days.indexOf(string.substr(indexOfDash + 1, 3));
-                for (let i = 0; i < days.length; i++) {
-                    if (i > start || i < end) {
-                        arraysOfDaysByNumber.push(i);
-                    }
-                }
-            }
+        let {arrayOfDaysByNumber, hours} = splitStringAndReturnDaysAndTimes(time);
+
         
-            if (days.includes(string)) {
-                arraysOfDaysByNumber.push(days.indexOf(string))
-            }
-        
-            if (splitString[stringIndex + 1] == 'am') {
-                let time = {}
-                let timeStringSplit = string.split(':');
-                time.hours = parseInt(timeStringSplit[0]);
-                time.minutes = parseInt(timeStringSplit[1]) || 0;
-                hours.push(time);
-            }
-        
-            if (splitString[stringIndex + 1] == 'pm') {
-                let time = {}
-                let timeStringSplit = string.split(':');
-                time.hours = parseInt(timeStringSplit[0]) + 12;
-                time.minutes = parseInt(timeStringSplit[1]) || 0;
-                hours.push(time);
-            }
-        }
-        for (let dayIndex = 0; dayIndex < arraysOfDaysByNumber.length; dayIndex++) {
-            let day = arraysOfDaysByNumber[dayIndex];
+        for (let dayIndex = 0; dayIndex < arrayOfDaysByNumber.length; dayIndex++) {
+            let day = arrayOfDaysByNumber[dayIndex];
             newTimesObj[day].open = true;
             newTimesObj[day].start = {...hours[0]};
             newTimesObj[day].end = {...hours[1]};
@@ -101,5 +68,47 @@ function createNewTimesObject(timesArray) {
     return newTimesObj;
 }
 
-let parsedData = parseRestaurantData([...json]);
+function splitStringAndReturnDaysAndTimes(string) {
+    let result = {
+        arrayOfDaysByNumber: [],
+        hours: []
+    }
+    let splitString = string.toLowerCase().split(' ');
+    for (let stringIndex = 0; stringIndex < splitString.length; stringIndex++) {
+        let string = splitString[stringIndex];
+        if (string.includes('-') && string.length > 1) {
+            let indexOfDash = string.indexOf('-');
+            let start = days.indexOf(string.substr(indexOfDash - 3, 3));
+            let end = days.indexOf(string.substr(indexOfDash + 1, 3));
+            for (let i = 0; i < days.length; i++) {
+                if (i >= start || i <= end) {
+                    result.arrayOfDaysByNumber.push(i);
+                }
+            }
+        }
+    
+        if (days.includes(string)) {
+            result.arrayOfDaysByNumber.push(days.indexOf(string))
+        }
+    
+        if (splitString[stringIndex + 1] == 'am') {
+            let time = {}
+            let timeStringSplit = string.split(':');
+            time.hours = parseInt(timeStringSplit[0]);
+            time.minutes = parseInt(timeStringSplit[1]) || 0;
+            result.hours.push(time);
+        }
+    
+        if (splitString[stringIndex + 1] == 'pm') {
+            let time = {}
+            let timeStringSplit = string.split(':');
+            time.hours = parseInt(timeStringSplit[0]) + 12;
+            time.minutes = parseInt(timeStringSplit[1]) || 0;
+            result.hours.push(time);
+        }
+    }
+    return result;
+}
+
+const parsedData = parseRestaurantData(restHours);
 export default parsedData;
